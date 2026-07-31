2ebeba8cad2bd1a475367a622497afb39d7deef0
Marcio Antonio dos Santos <marcioasan@gmail.com>
2026-07-31 16:01:58 -0300

    8.8. Isolando execução dos testes de integração

---
+++ src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
@@ -0,0 +1,14 @@
+package com.algaworks.algashop.ordering.infrastructure.persistence.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//8.8. Isolando execução dos testes de integração - 9'40"
+@SpringBootTest
+class OrderPersistenceEntityRepositoryIT {
+
+    @Test
+    public void shouldRun() {
+
+    }
+}
