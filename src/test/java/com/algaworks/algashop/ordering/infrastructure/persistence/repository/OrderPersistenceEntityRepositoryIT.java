a8513a25ce536d9e16debbd85626fb54fc7fd0e8
Marcio Antonio dos Santos <marcioasan@gmail.com>
2026-07-31 16:36:13 -0300

    8.9. Testando modelo de persistência com testes de integração

--- src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
+++ src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
@@ -1,14 +1,51 @@
 package com.algaworks.algashop.ordering.infrastructure.persistence.repository;
 
+import com.algaworks.algashop.ordering.domain.model.utility.IdGenerator;
+import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 
 //8.8. Isolando execução dos testes de integração - 9'40"
+//8.9. Testando modelo de persistência com testes de integração - 1' - mostra como executar test ou integrationTest
 @SpringBootTest
+@Transactional //8.9. Testando modelo de persistência com testes de integração - 7'30"
 class OrderPersistenceEntityRepositoryIT {
 
-    @Test
-    public void shouldRun() {
+    private final OrderPersistenceEntityRepository orderPersistenceEntityRepository;
 
+    @Autowired
+    public OrderPersistenceEntityRepositoryIT(OrderPersistenceEntityRepository orderPersistenceEntityRepository) {
+        this.orderPersistenceEntityRepository = orderPersistenceEntityRepository;
+    }
+
+    //8.9. Testando modelo de persistência com testes de integração - 3'
+    @Test
+    public void shouldPersist() {
+        long orderId = IdGenerator.generateTSID().toLong();
+        OrderPersistenceEntity entity = OrderPersistenceEntity.builder()
+                .id(orderId)
+                .customerId(IdGenerator.generateTimeBasedUUID())
+                .totalItems(2)
+                .totalAmount(new BigDecimal(1000))
+                .status("DRAFT")
+                .paymentMethod("CREDIT_CARD")
+                .placedAt(OffsetDateTime.now())
+                .build();
+
+        orderPersistenceEntityRepository.saveAndFlush(entity);
+        Assertions.assertThat(orderPersistenceEntityRepository.existsById(orderId)).isTrue();
+    }
+
+    //8.9. Testando modelo de persistência com testes de integração - 5'30"
+    @Test
+    public void shouldCount() {
+        long ordersCount = orderPersistenceEntityRepository.count();
+        Assertions.assertThat(ordersCount).isZero();
     }
 }
