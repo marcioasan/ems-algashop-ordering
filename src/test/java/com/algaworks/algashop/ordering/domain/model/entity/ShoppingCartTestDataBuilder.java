5dddaa9b9811ee609289c463c5b805877a230c7e
Marcio Antonio dos Santos <marcioasan@gmail.com>
2026-09-13 22:07:10 -0300

    9.7. Como lidar com atualizações em massa Erros corrigidos e testes funcionando.

--- src/test/java/com/algaworks/algashop/ordering/domain/model/entity/ShoppingCartTestDataBuilder.java
+++ src/test/java/com/algaworks/algashop/ordering/domain/model/entity/ShoppingCartTestDataBuilder.java
@@ -6,7 +6,8 @@ import com.algaworks.algashop.ordering.domain.model.valueobject.id.ShoppingCartI
 
 public class ShoppingCartTestDataBuilder {
 
-    public CustomerId customerId = new CustomerId();
+//    public CustomerId customerId = new CustomerId(); deixando dessa forma, causava "Referential integrity constraint violation" nos testes
+    public CustomerId customerId = CustomerTestDataBuilder.DEFAULT_CUSTOMER_ID;
     public static final ShoppingCartId DEFAULT_SHOPPING_CART_ID = new ShoppingCartId();
     private boolean withItems = true;
 
